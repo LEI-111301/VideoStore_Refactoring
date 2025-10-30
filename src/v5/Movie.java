b852d0a07e204f98923779576e157305cec5bbc3
LEI-110582 <st3ps.pro@gmail.com>
2025-10-30 21:16:31 +0000

    from V5 to V6 using Move Methods

--- src/v5/Movie.java
+++ src/v5/Movie.java
@@ -2,6 +2,34 @@ package v5;
 
 public class Movie {
 
+	public double getAmount(Rental rental)
+	{
+		double result = 0;
+
+		switch (rental.getMovie().getPriceCode())
+		{
+			case REGULAR:
+				result += 2;
+				if (rental.getDaysRented() > 2)
+					result += (rental.getDaysRented() - 2) * 1.5;
+				break;
+			case NEW_RELEASE:
+				result += rental.getDaysRented() * 3;
+				break;
+			case CHILDRENS:
+				result += 1.5;
+				if (rental.getDaysRented() > 3)
+					result += (rental.getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return result;
+	}
+
+	public int getFrequentRentalPoints(Rental rental)
+	{
+		return (rental.getMovie().getPriceCode() == Code.NEW_RELEASE) && rental.getDaysRented() > 1 ? 2 : 1;
+	}
+
 	public enum Code {REGULAR, CHILDRENS, NEW_RELEASE};
 	
 	private String _title;
