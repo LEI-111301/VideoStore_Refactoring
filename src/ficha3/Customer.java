c6cff997a9b39e44aee4a25f6e2a2fe4a3944ef3
LEI-130485 <dggoo@iscte-iul.pt>
2025-10-30 15:02:26 +0000

    v3

--- src/ficha3/Customer.java
+++ src/ficha3/Customer.java
@@ -2,53 +2,50 @@ package ficha3;
 
 import java.util.Vector;
 
-public class Customer
-{
-	private String			_name;
-	private Vector<Rental>	_rentals	= new Vector<Rental>();
+public class Customer {
+    private String _name;
+    private Vector<Rental> _rentals = new Vector<Rental>();
 
-	public Customer(String _name)
-	{
-		this._name = _name;
-	}
+    public Customer(String _name) {
+        this._name = _name;
+    }
 
-	public void addRental(Rental arg)
-	{
-		_rentals.addElement(arg);
-	}
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
 
-	public String getName()
-	{
-		return _name;
-	}
+    public String getName() {
+        return _name;
+    }
 
-	public String statement()
-	{
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
+    // Novo método para calcular pontos de frequent renter
+    private int getFrequentRenterPoints(Rental rental) {
+        int points = 1; // 1 ponto por cada aluguer
+        if ((rental.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && rental.getDaysRented() > 1) {
+            points++; // ponto extra para novos lançamentos alugados mais de 1 dia
+        }
+        return points;
+    }
 
-		// header
-		String result = "Rental Record for " + getName() + "\n";
-		
-		for (Rental each: _rentals)
-		{
-            double thisAmount = getAmount(each);
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
 
-            // add frequent renter points
-			frequentRenterPoints++;
+        String result = "Rental Record for " + getName() + "\n";
 
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
+        for (Rental each : _rentals) {
+            // Inline do cálculo do valor do aluguer
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
+            totalAmount += each.getAmount();
 
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
-		}
+            // calcular pontos de frequent renter usando o método extraído
+            frequentRenterPoints += getFrequentRenterPoints(each);
+        }
 
-		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
-		return result;
-	}
+        // footer
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+
+        return result;
+    }
 }
